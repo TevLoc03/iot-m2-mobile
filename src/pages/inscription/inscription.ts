b3f3c23f5d66c6e515ac23061a04b50c46bd8edf
TevBarbaRossa <t.locandro@gmail.com>
2017-12-12 09:14:16 +0100

    merge

--- src/pages/inscription/inscription.ts
+++ src/pages/inscription/inscription.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CommandePage } from '../commande/commande';
+import { AlertController } from 'ionic-angular';
 
 
 @Component({
@@ -8,8 +10,17 @@ import { NavController } from 'ionic-angular';
 })
 export class InscriptionPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
     
   }
+  valider() {
+    this.navCtrl.push(CommandePage);
+      let alert = this.alertCtrl.create({
+        title: 'Inscription validé !',
+        subTitle: 'Un mail vous à été envoyé, veuillez suivre les instructions si vous voulez créditer votre carte.<br>  Merci !',
+        buttons: ['OK']
+      });
+      alert.present();
+    }
 
 }
